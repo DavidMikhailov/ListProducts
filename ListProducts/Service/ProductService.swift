1e1940a8f016b06c22aa04ebcf00adbf2fb5a2c0
David Mikhailov <david.mikhailovv@gmail.com>
2021-02-23 10:12:27 +0400

    Database

--- ListProducts/Service/ProductService.swift
+++ ListProducts/Service/ProductService.swift
@@ -6,52 +6,115 @@
 //
 
 import Foundation
+import RealmSwift
 
-protocol ProductServiceProtocol: class {
+protocol ProductsServiceProtocol: class {
     /// Create
     func create(product: Product, onComplete: @escaping (Error?) -> Void)
     
     /// Read
     func getProductList(onComplete: @escaping ([Product]) -> Void)
     func getProduct(id: Product.Id, onComplete: @escaping (Product) -> Void)
-    
+
     /// Update
     func edit(product: Product, onComplete: @escaping (Error?) -> Void)
-    
+
     /// Delete
     func delete(id: Product.Id, onComplete: @escaping (Error?) -> Void)
 }
 
-class ProductService: ProductServiceProtocol {
+class ProductsService: ProductsServiceProtocol {
     
-    let realm: RealmDB
+    private let database: ProductDatabase
+    private let bgQueue = DispatchQueue.global(qos: .background)
     
+    init(database: ProductDatabase) {
+        self.database = database
+    }
+
     /// Create
     func create(product: Product, onComplete: @escaping (Error?) -> Void) {
-        
-    }
-    
-    /// Read
-    func getProductList(onComplete: @escaping ([Product]) -> Void) {
-        realm.readList() { objects in
-            let products = objects.map {}
-            complete(products)
+        bgQueue.async {
+            let product = ProductRealmModel(
+                id: product.id,
+                name: product.name,
+                price: product.price,
+                count: product.count,
+                imageData: product.image?.jpegData(compressionQuality: 0.2),
+                dateCreated: Date()
+            )
+            do {
+                try self.database.create(product: product)
+                onComplete(nil)
+            } catch {
+                onComplete(error)
+            }
         }
     }
-    func getProduct(id: Product.Id, onComplete: @escaping (Product) -> Void) {
-        
-    }
     
+    func getProductList(onComplete: @escaping ([Product]) -> Void) {
+        bgQueue.async {
+            let results = self.database.readAll().sorted(byKeyPath: "dateCreated")
+            let models = results.map { realmModel in
+                Product.make(from: realmModel)
+            }
+            onComplete(Array(models))
+        }
+    }
+        
+    func getProduct(id: Product.Id, onComplete: @escaping (Product) -> Void) {
+        bgQueue.async {
+            let predicate = NSPredicate(format: "id = %@", id)
+            let results = self.database.readAll().filter(predicate)
+            let product = Product.make(from: results.first!)
+            onComplete(product)
+        }
+    }
+
     /// Update
     func edit(product: Product, onComplete: @escaping (Error?) -> Void) {
-        
+        bgQueue.async {
+            let product = ProductRealmModel(
+                id: product.id,
+                name: product.name,
+                price: product.price,
+                count: product.count,
+                imageData: product.image?.jpegData(compressionQuality: 0.2),
+                dateCreated: product.dateCreated
+            )
+            do {
+                try self.database.edit(product: product)
+                onComplete(nil)
+            } catch {
+                onComplete(error)
+            }
+        }
     }
-    
+
     /// Delete
     func delete(id: Product.Id, onComplete: @escaping (Error?) -> Void) {
-        
+        bgQueue.async {
+            do {
+                try self.database.delete(productId: id)
+                onComplete(nil)
+            } catch {
+                onComplete(error)
+            }
+        }
     }
-    
 }
 
    
+
+extension Product {
+    static func make(from realmModel: ProductRealmModel) -> Product {
+        Product(
+            id: realmModel.id,
+            dateCreated: realmModel.dateCreated!,
+            name: realmModel.name,
+            price: Decimal(realmModel.price),
+            count: realmModel.count,
+            image: realmModel.imageData.flatMap { UIImage(data: $0) }
+        )
+    }
+}
